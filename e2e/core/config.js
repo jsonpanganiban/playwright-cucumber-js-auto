2359ac476c6b6f4e62fb262bf40dd96d2205f412
jsonpanganiban <json.panganiban@gmail.com>
2022-07-15 14:19:50 +1000

    updated config

--- e2e/core/config.js
+++ e2e/core/config.js
@@ -1,6 +1,11 @@
 const browserOptions = {
   slowMo: 0,
-  args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
+  args: [
+    '--use-fake-ui-for-media-stream',
+    '--use-fake-device-for-media-stream',
+    '--no-sandbox',
+    '--disable-dev-shm-usage',
+  ],
   firefoxUserPrefs: {
     'media.navigator.streams.fake': true,
     'media.navigator.permission.disabled': true,
@@ -12,9 +17,13 @@ export const config = {
   browser: process.env.BROWSER || 'chromium',
   browserOptions,
   IMG_THRESHOLD: { threshold: 0.4 },
-  BASE_API_URL: 'https://www.demo.bnz.co.nz/client/',
+  recordVideos: process.env.PWVIDEO || false,
+  defaultTimeout: 60 * 1000, // milliseconds
+  runSlow: 1000, // milliseconds
 };
 
 Object.assign(global, {
-  BASE_URL: 'https://www.demo.bnz.co.nz/client/',
+  BASE_URL: process.env.BASE_URL || 'https://www.demo.bnz.co.nz/client/',
 });
+
+module.exports = { config };
